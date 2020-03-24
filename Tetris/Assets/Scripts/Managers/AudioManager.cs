1ae9ffcef8ea5997ec43d943072246e2cb112394
IronFatalis <52928832+IronFatalis@users.noreply.github.com>
2020-03-24 17:56:55 -0400

    AudioManager & option screen
    Built AudioManager and have music playing between each screen. Also have Option screen mostly set up, just need to fix up arrow locations and make a var for types.

---
+++ Tetris/Assets/Scripts/Managers/AudioManager.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    public AudioClip Music0;
+    public AudioClip Music1;
+    public AudioClip Music2;
+    public AudioClip Music3;
+
+    private AudioSource audioSource;
+
+    private int CurrentMusic = 0;
+    private int NewMusic = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+        audioSource = gameObject.GetComponent<AudioSource>();
+        audioSource.PlayOneShot(Music0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (NewMusic != SelectType.SelectMusic)
+        {
+            print(SelectType.SelectMusic);
+            NewMusic = SelectType.SelectMusic;
+        }
+        if (CurrentMusic != NewMusic)
+        {
+            audioSource.Stop();
+            audioSource.loop = true;
+            CurrentMusic = NewMusic;
+
+            if (CurrentMusic == 0)
+            {
+                audioSource.PlayOneShot(Music0);
+            }
+            else if (CurrentMusic == 1)
+            {
+                audioSource.PlayOneShot(Music1);
+            }
+            else if (CurrentMusic == 2)
+            {
+                audioSource.PlayOneShot(Music2);
+            }
+            else if (CurrentMusic == 3)
+            {
+                audioSource.PlayOneShot(Music3);
+            }
+            
+        }
+    }
+}
--- Tetris/Assets/Scripts/SelectType.cs
+++ Tetris/Assets/Scripts/SelectType.cs
@@ -7,30 +7,56 @@ public class SelectType : MonoBehaviour
 {
     private GameObject typeCursorLeft;
     private GameObject typeCursorRight;
-    private int StartingLocation;
-    private int TypeBDistance = 100;
+    private GameObject musicCursorLeft;
+    private GameObject musicCursorRight;
 
+    private Vector2 NewMusicLocationLeft = new Vector2(153, 130.5f);
+    private Vector2 NewMusicLocationRight = new Vector2(261, 130.5f);
+
+    private double BottomMusic = 57;
+    private double TopMusic = 130.5f;
+    private float MusicDiffernce = 24.5f;
+
+    public static int SelectMusic = 0;
 
     // Start is called before the first frame update
     void Start()
     {
         typeCursorLeft = GameObject.Find("TypeCursorLeft");
         typeCursorRight = GameObject.Find("TypeCursorRight");
-        StartingLocation = -91;
+        musicCursorLeft = GameObject.Find("MusicCursorLeft");
+        musicCursorRight = GameObject.Find("MusicCursorRight");
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.LeftArrow) && this.transform.position.x < StartingLocation)    // & cursor not already left
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            typeCursorLeft.transform.position.x = StartingLocation + TypeBDistance;    //Move cursor left
-            typeCursorRight.transform.position.x = StartingLocation + TypeBDistance;    //Move cursor left
+            typeCursorLeft.transform.position = new Vector2(94,253);
+            typeCursorRight.transform.position = new Vector2(180, 253);
         }
-        else if (Input.GetKeyDown(KeyCode.RightArrow) && this.transform.position.x > StartingLocation)   // & cursor not already right
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
         {
-            typeCursorLeft.transform.position.x = StartingLocation;    //Move cursor right
-            typeCursorRight.transform.position.x = StartingLocation;    //Move cursor right
+            typeCursorLeft.transform.position = new Vector2(234, 252);
+            typeCursorRight.transform.position = new Vector2(320, 252);
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow) && NewMusicLocationLeft.y > BottomMusic)
+        {
+            NewMusicLocationLeft.y = NewMusicLocationLeft.y - MusicDiffernce;
+            NewMusicLocationRight.y = NewMusicLocationRight.y - MusicDiffernce;
+            musicCursorLeft.transform.position = NewMusicLocationLeft;
+            musicCursorRight.transform.position = NewMusicLocationRight;
+            SelectMusic++;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow) && NewMusicLocationLeft.y < TopMusic)
+        {
+            NewMusicLocationLeft.y = NewMusicLocationLeft.y + MusicDiffernce;
+            NewMusicLocationRight.y = NewMusicLocationRight.y + MusicDiffernce;
+            musicCursorLeft.transform.position = NewMusicLocationLeft;
+            musicCursorRight.transform.position = NewMusicLocationRight;
+            SelectMusic--;
         }
 
         if (Input.GetKeyDown(KeyCode.Return))
