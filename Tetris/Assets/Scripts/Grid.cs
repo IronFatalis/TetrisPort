4b42638346366447a773bc8d9c2a9b63e15dbf6c
IronFatalis <52928832+IronFatalis@users.noreply.github.com>
2020-03-25 15:32:39 -0400

    Game Piece moves (no grid yet)
    Game piece moves all directions on gameScreen, need to attach it to a grid still, piece size is correct for the proper amount of cells.

--- Tetris/Assets/Scripts/Grid.cs
+++ Tetris/Assets/Scripts/Grid.cs
@@ -7,8 +7,24 @@ public class Grid : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
-    }
+		for (int i = 0; i < 24; i++)
+		{
+			for (int j = 0; j < 10; j++)
+			{
+				//if (pieceRow == j && pieceColumn == i)
+				//{
+					for (int z = 0; z < 4; z++)
+					{
+						//GridPiece[z][mColumns][mRows] = new Texture("GameSprites.png", 38, 160, 7, 7);
+						//GridPiece[z][mColumns][mRows]->SetParent(this);
+						//GridPiece[z][mColumns][mRows]->SetScale(Vector2(3.5f, 3.5f));
+					}
+					//Tetromino			PieceShape();
+					//Movement			GridPiece[p][mColumns][mRows]->SetPosition(Graphics::SCREEN_WIDTH * 0.395f + ((pieceRow * slotSize) + (slotSize * oldPos[p].x)), Graphics::SCREEN_HEIGHT * 0.185f + (pieceColumn * slotSize) + (slotSize * oldPos[p].y));
+				//}
+			}
+		}
+	}
 
     // Update is called once per frame
     void Update()
---
+++ Tetris/Assets/Scripts/PieceMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PieceMovement : MonoBehaviour
+{
+    private GameObject currentPiece;
+    
+    private float CellSize = 10f;
+    private Vector2 Tetromino = new Vector2(180f, 170f);
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentPiece = GameObject.Find("CurrentPiece");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            Tetromino.x = Tetromino.x - CellSize;
+            currentPiece.transform.position = Tetromino;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            Tetromino.x = Tetromino.x + CellSize;
+            currentPiece.transform.position = Tetromino;
+        }
+
+        if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            Tetromino.y = Tetromino.y - CellSize;
+            currentPiece.transform.position = Tetromino;
+        }
+        else if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            Tetromino.y = Tetromino.y + CellSize;
+            currentPiece.transform.position = Tetromino;
+        }
+    }
+}
