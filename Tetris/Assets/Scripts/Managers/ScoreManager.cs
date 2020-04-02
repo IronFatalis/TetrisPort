1363fb37fa7cdb91268bbee9b9ef058a3d446e39
IronFatalis <52928832+IronFatalis@users.noreply.github.com>
2020-04-02 14:45:21 -0400

    font and score test
    Added font and testing adding number text in for score

---
+++ Tetris/Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+    public Text PlayerScore;
+    public int NumPlayerScore = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        PlayerScore.GetComponent<UnityEngine.UI.Text>().text = NumPlayerScore.ToString();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
