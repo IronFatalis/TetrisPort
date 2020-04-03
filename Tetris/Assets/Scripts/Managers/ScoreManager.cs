334435cf7567d5f7c36a1fb9d020a0e8e2a3ff4e
IronFatalis <52928832+IronFatalis@users.noreply.github.com>
2020-04-03 12:49:36 -0400

    Score variables update, just need drawn
    All score areas update properly and converts from int to string and back. Score will increase with "up" key (for now). Just need placement of actual text on screen.
    
    (Forgot to push before ending last night)

--- Tetris/Assets/Scripts/Managers/ScoreManager.cs
+++ Tetris/Assets/Scripts/Managers/ScoreManager.cs
@@ -2,21 +2,63 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using System;
 
-public class Score : MonoBehaviour
+public class ScoreManager : MonoBehaviour
 {
-    public Text PlayerScore;
-    public int NumPlayerScore = 0;
+    private GameObject currentScore;
+
+    public static string StrCurrentScore = "0";
+    public int NumCurrentScore = Int32.Parse(StrCurrentScore);
+
+    private static string StrLevel = "01";
+    private int NumLevel = Int32.Parse(StrLevel);
+
+    private static string StrHiScore = "100000";
+    private int NumHiScore = Int32.Parse(StrHiScore);
+
+    private static string StrLine = "00";
+    private int NumLine = Int32.Parse(StrLine);
+
+    private static string StrTStat = "0";
+    static public int NumTStat = Int32.Parse(StrTStat);
+
+    private static string StrJStat = "0";
+    private int NumJStat = Int32.Parse(StrJStat);
+
+    private static string StrZStat = "0";
+    private int NumZStat = Int32.Parse(StrZStat);
+
+    private static string StrOStat = "0";
+    private int NumOStat = Int32.Parse(StrOStat);
+
+    private static string StrSStat = "0";
+    private int NumSStat = Int32.Parse(StrSStat);
+
+    private static string StrLStat = "0";
+    private int NumLStat = Int32.Parse(StrLStat);
+
+    private static string StrIStat = "0";
+    private int NumIStat = Int32.Parse(StrIStat);
+
 
     // Start is called before the first frame update
     void Start()
     {
-        PlayerScore.GetComponent<UnityEngine.UI.Text>().text = NumPlayerScore.ToString();
+        currentScore = GameObject.Find("PlayerScore");
+        //NumCurrentScore.GetComponent<UnityEngine.UI.Text>().text = NumCurrentScore.ToString();
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            NumCurrentScore = Int32.Parse(StrCurrentScore);
+            NumCurrentScore += 1000;
+            StrCurrentScore = NumCurrentScore.ToString().PadLeft(7,'0');
+            print(StrCurrentScore);
+            currentScore.GetComponent<UnityEngine.UI.Text>().text = StrCurrentScore;
+        }
     }
 }
--- Tetris/Assets/Scripts/RandomGenerator.cs
+++ Tetris/Assets/Scripts/RandomGenerator.cs
@@ -7,21 +7,28 @@ public class RandomGenerator : MonoBehaviour
     public static int mRandomPieceCurrent = 0;
     public static int mRandomPieceNext = 0;
 
-    // Start is called before the first frame update
-    void Start()
+	private GameObject playerScore;
+
+	// Start is called before the first frame update
+	void Start()
     {
         SetNextPiece();
         SetCurrentPiece();
         SetNextPiece();
-    }
+
+		playerScore = GameObject.Find("PlayerScore");
+	}
 
     // Update is called once per frame
     void Update()
     {
-        // if piece locks
-        //setCurrentPiece()
-        //SetNextPiece();
-    }
+		// if piece locks
+		//setCurrentPiece()
+		//SetNextPiece();
+
+
+		
+	}
 
     int GetNextPiece()
     {
@@ -41,5 +48,37 @@ public class RandomGenerator : MonoBehaviour
     void SetCurrentPiece()
     {
         mRandomPieceCurrent = mRandomPieceNext;
-    }
+
+		//switch (mRandomPieceCurrent)
+		//{
+		//	case 0:
+		//		playerScore.NumTStat += 1;
+		//		ScoreManager.StrTStat = ScoreManager.NumTStat.ToString();
+		//		break;
+		//	case 1:
+		//		ScoreManager.NumJStat += 1;
+		//		ScoreManager.StrJStat = ScoreManager.NumJStat.ToString();
+		//		break;
+		//	case 2:
+		//		ScoreManager.NumZStat += 1;
+		//		ScoreManager.StrZStat = ScoreManager.NumZStat.ToString();
+		//		break;
+		//	case 3:
+		//		ScoreManager.NumOStat += 1;
+		//		ScoreManager.StrOStat = ScoreManager.NumOStat.ToString();
+		//		break;
+		//	case 4:
+		//		ScoreManager.NumSStat += 1;
+		//		ScoreManager.StrSStat = ScoreManager.NumSStat.ToString();
+		//		break;
+		//	case 5:
+		//		ScoreManager.NumLStat += 1;
+		//		ScoreManager.StrLStat = ScoreManager.NumLStat.ToString();
+		//		break;
+		//	case 6:
+		//		ScoreManager.NumIStat += 1;
+		//		ScoreManager.StrIStat = ScoreManager.NumIStat.ToString();
+		//		break;
+		//}
+	}
 }
